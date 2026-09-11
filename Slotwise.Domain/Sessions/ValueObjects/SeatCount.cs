d7ae4584d8e8ff2ecf896d1b167646bf4394fd3e
Mohamed Abdi <khadar.abdi165@gmail.com>
2026-09-11 11:15:31 +0200

    domain files

---
+++ Slotwise.Domain/Sessions/ValueObjects/SeatCount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slotwise.Domain.Sessions.ValueObjects
+{
+    internal class SeatCount
+    {
+    }
+}
